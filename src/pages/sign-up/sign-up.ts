9253083473a704afe025bde9d248b5177cd7a260
Candide Kemmler <ckemmler@gmail.com>
2017-04-14 22:26:00 +0200

    improves sign up page (#4)

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -19,6 +19,10 @@ export class SignUpPage {
 
   signup: FormGroup;
   main_page: { component: any };
+  username: FormControl;
+  email: FormControl;
+  password: FormControl;
+  confirm_password: FormControl;
 
   static instance: SignUpPage;
 
@@ -31,12 +35,12 @@ export class SignUpPage {
     this.main_page = { component: TopicsPage };
 
     this.signup = new FormGroup({
-      email: new FormControl('', [Validators.required]),
-      username: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z0-9]+$/)]),
+      email: this.email = new FormControl('', [Validators.required]),
+      username: this.username = new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z0-9]+$/)]),
       firstname: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]),
-      lastname: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]),
-      password: new FormControl('xxxxxxxx', [Validators.required, Validators.minLength(8), Validators.maxLength(24)]),
-      confirm_password: new FormControl('xxxxxxxx', Validators.required)
+      lastname: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z ]+$/)]),
+      password: this.password = new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(24)]),
+      confirm_password: this.confirm_password = new FormControl('', Validators.required)
     });
 
     SignUpPage.instance = this;
@@ -44,6 +48,10 @@ export class SignUpPage {
 
   doSignup(){
     console.log(this.signup.value);
+    if (this.password.value!=this.confirm_password.value) {
+      SignUpPage.alert("Passwords don't match");
+      return;
+    }
     this.loginService.signUp(this.signup.value).then(
       (signUpObs)=> {
         signUpObs
@@ -56,19 +64,31 @@ export class SignUpPage {
     );
   }
 
-  static showError() {
+  static showError(error: Response) {
+    SignUpPage.instance.logger.log("logging error " + error.status + ", " + error.statusText, error);
+    SignUpPage.instance.logger.error("handling error", error);
+    let errorObj = error.json();
+    let message: string = "Please verify email, username and passwords";
+    if (errorObj.email)
+      message = errorObj.email;
+    else if (errorObj.username)
+      message = errorObj.username;
+    else if (errorObj.password)
+      message = errorObj.password;
+    SignUpPage.alert(message);
+  }
+
+  static alert(message) {
     let alert = SignUpPage.instance.alertCtrl.create({
-      title: 'Server Error',
-      subTitle: 'Please verify email, username and passwords',
+      title: 'Signup failure',
+      subTitle: message,
       buttons: ['Dismiss']
     });
     alert.present();
   }
 
   public handleError(error: Response) {
-    console.log("logging error " + error.status + ", " + error.statusText, error);
-    console.error("handling error", error);
-    SignUpPage.showError();
+    SignUpPage.showError(error);
   }
 
   showTermsModal() {
